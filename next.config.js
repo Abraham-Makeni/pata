2acb0a0c48d240206f232599a56bb7fb32799f1e
bhunnid <brandonkkiplangat@gmail.com>
2026-04-27 12:33:15 +0300

    FIXES

--- next.config.js
+++ next.config.js
@@ -16,7 +16,6 @@ const nextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
     dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   skipTrailingSlashRedirect: true
 }
